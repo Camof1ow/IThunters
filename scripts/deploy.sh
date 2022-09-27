1415240a277b362ebe3967a626a38b20f2f9ff07
Camof1ow <astk1203@nate.com>
2022-09-27 12:47:14 +0900

    [기능개발] 무중단배포 shell script 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,24 +12,43 @@ echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
 
 echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
 
-CURRENT_PID=$(lsof -t -i:8000)
 
-if [ -z $CURRENT_PID ]
+
+echo "> 현재 구동중인 Set 확인" >> $DEPLOY_LOG_PATH
+CURRENT_PROFILE=$(curl -s http://localhost/profile)
+echo  ">>>>>> Set : $CURRENT_PROFILE" >> $DEPLOY_LOG_PATH
+
+if [ ${CURRENT_PROFILE} == 1 ]
 then
-  echo "> 현재 동작중인 어플리케이션 존재 X" >> $DEPLOY_LOG_PATH
+  PROFILE_SET="set2"
+  IDLE_PORT=8082
+elif [ ${CURRENT_PROFILE}  == 2 ]
+then
+  PROFILE_SET="set1"
+  IDLE_PORT=8081
 else
-
-  echo "> 현재 동작중인 어플리케이션 존재 O" >> $DEPLOY_LOG_PATH
-  echo "> 현재 동작중인 어플리케이션 강제 종료 진행" >> $DEPLOY_LOG_PATH
-  echo "> kill -9 $CURRENT_PID" >> $DEPLOY_LOG_PATH
-  kill -9 $(lsof -t -i:8000)
+  echo "> 일치하는 Profile이 없습니다. Profile: $CURRENT_PROFILE" >> $DEPLOY_LOG_PATH
+  echo "> 8081을 할당합니다." >> $DEPLOY_LOG_PATH
+  IDLE_PORT=8081
 fi
 
+echo ""
+
+echo "> 전환할 Port: $IDLE_PORT" >> $DEPLOY_LOG_PATH
+echo "> Port 전환" >> $DEPLOY_LOG_PATH
+echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" |sudo tee /etc/nginx/conf.d/service-url.inc >> $DEPLOY_LOG_PATH
+
+PROXY_PORT=$(curl -s http://localhost/profile)
+echo "> Nginx Current Proxy Port: $PROXY_PORT" >> $DEPLOY_LOG_PATH
+
+echo "> Nginx Reload" >> $DEPLOY_LOG_PATH
+sudo service nginx reload # reload는 설정만 재적용하기 때문에 바로 적용이 가능합니다.
 
 echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
 cd /home/ubuntu/itmonster/deploy/
 chmod +x ITsquad-0.0.1-SNAPSHOT.jar
-nohup java -jar -Duser.timezone=Asia/Seoul /home/ubuntu/itmonster/deploy/ITsquad-0.0.1-SNAPSHOT.jar --spring.config.location=./set1.properties --server.port=8000 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
+# shellcheck disable=SC2046
+nohup java -jar -Duser.timezone=Asia/Seoul /home/ubuntu/itmonster/deploy/ITsquad-0.0.1-SNAPSHOT.jar --spring.config.location=./${PROFILE_SET}.properties --server.port=${IDLE_PORT} >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
 
 sleep 3
 
---
+++ scripts/switch.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+SWITCH_LOG_PATH="/home/ubuntu/itmonster/switch.log"
+
+echo "> 현재 구동중인  Port 확인" >> $SWITCH_LOG_PATH
+CURRENT_PROFILE=$(curl -s http://localhost/profile)
+echo "> ${CURRENT_PROFILE}" >> $SWITCH_LOG_PATH
+if [ $CURRENT_PROFILE == 1 ]
+then
+  IDLE_PORT=8082
+elif [ $CURRENT_PROFILE == 2 ]
+then
+  IDLE_PORT=8081
+else
+  echo "> 일치하는 Profile이 없습니다. Profile: $CURRENT_PROFILE" >> $SWITCH_LOG_PATH
+  echo "> 8081을 할당합니다." >> $SWITCH_LOG_PATH
+  IDLE_PORT=8081
+fi
+
+echo "> 전환할 Port: $IDLE_PORT" >> $SWITCH_LOG_PATH
+echo "> Port 전환" >> $SWITCH_LOG_PATH
+echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" |sudo tee /etc/nginx/conf.d/service-url.inc >> $SWITCH_LOG_PATH
+
+PROXY_PORT=$(curl -s http://localhost/profile)
+echo "> Nginx Current Proxy Port: $PROXY_PORT" >> $SWITCH_LOG_PATH
+
+echo "> Nginx Reload" >> $SWITCH_LOG_PATH
+sudo service nginx reload # reload는 설정만 재적용하기 때문에 바로 적용이 가능합니다.
