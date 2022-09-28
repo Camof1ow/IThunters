6abb894772210cd23376a16bb13bcc10eabedbec
Camof1ow <astk1203@nate.com>
2022-09-28 23:28:49 +0900

    [fix] 카카오 로그인 시 이메일 변조

--- src/main/java/com/example/itmonster/service/KakaoUserService.java
+++ src/main/java/com/example/itmonster/service/KakaoUserService.java
@@ -179,12 +179,7 @@ public class KakaoUserService {
 
 		// null값 오면 회원가입 가능 기존 사용자가 없다는 뜻
 		if (kakaoUser == null) {  // 회원가입
-			String email = "";
-			if (memberRepository.existsByEmail(kakaoUserInfo.getEmail())) {
-				email += "KAKAO-"+kakaoUserInfo.getEmail();
-			}else {
-				email += kakaoUserInfo.getEmail();
-			}
+			String email = "KAKAO-"+kakaoUserInfo.getEmail();
 
 			String nickname = kakaoUserInfo.getNickname();
 			if (memberRepository.existsByNickname(nickname)) {
