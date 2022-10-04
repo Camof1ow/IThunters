f96800105c3f3cfecc07708b1d72ed65f38d18f7
oneqrhee <blaackpurin@gmail.com>
2022-10-04 14:13:45 +0900

    [fix] 채널 멤버조회 오류수정

--- src/main/java/com/example/itmonster/socket/MessageController.java
+++ src/main/java/com/example/itmonster/socket/MessageController.java
@@ -23,15 +23,20 @@ public class MessageController {
         return messageService.readMessages(channelId);
     }
 
+    @GetMapping("/api/channels/{channelId}/test")
+    public List<MessageResponseDto> readMessagesTest(@PathVariable Long channelId) {
+        return messageService.readMessagesTest(channelId);
+    }
+
     @MessageMapping(value = {"/channels/{channelId}"})
-    public void addMessage(@RequestBody MessageRequestDto messageRequestDto,
-        @DestinationVariable Long channelId,
-        @Header("Authorization") String token) {
-        System.out.println(token);
-        token = token.substring(7);
-//    public void sendMessage(@RequestBody MessageRequestDto messageRequestDto,
-//        @DestinationVariable Long channelId) {
-//        String token = messageRequestDto.getToken().substring(7); // 백엔드 테스트용
+//    public void addMessage(@RequestBody MessageRequestDto messageRequestDto,
+//        @DestinationVariable Long channelId,
+//        @Header("Authorization") String token) {
+//        System.out.println(token);
+//        token = token.substring(7);
+    public void sendMessage(@RequestBody MessageRequestDto messageRequestDto,
+        @DestinationVariable Long channelId) {
+        String token = messageRequestDto.getToken().substring(7); // 백엔드 테스트용
         messageService.sendMessage(messageRequestDto, channelId, token);
     }
 }
